1fc3d29937807d331a743e9fc8bc425b47aa1242
Nitesh <niteshp27@gmail.com>
2021-09-11 22:49:45 +0530

    recaptcha issues

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -5,9 +5,12 @@ import { serialize } from 'dom-form-serializer'
 import Recaptcha from 'react-google-recaptcha'
 import './Form.css'
 
-const RECAPTCHA_KEY = process.env.APP_SITE_RECAPTCHA_KEY
+const RECAPTCHA_KEY = process.env.NETLIFY_SITE_RECAPTCHA_KEY
+if (!process.env.NETLIFY_SITE_RECAPTCHA_KEY) {
+  RECAPTCHA_KEY = "6LfP01wcAAAAAJg6jgTdFFdl0DocIwYP8x_Jqrfb"
+}
 if (typeof RECAPTCHA_KEY === 'undefined') {
-  throw new Error(`Env var APP_SITE_RECAPTCHA_KEY is undefined! You probably forget to set it in your Netlify build environment variables. Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings Note this demo is specifically for Recaptcha v2 `)
+  throw new Error(`Env var NETLIFY_SITE_RECAPTCHA_KEY is undefined! You probably forget to set it in your Netlify build environment variables. Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings Note this demo is specifically for Recaptcha v2 `)
 }
 
 class Form extends React.Component {
