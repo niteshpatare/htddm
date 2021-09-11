4f498e062441d5e8642b0d840bce291f04730c6f
Nitesh <niteshp27@gmail.com>
2021-09-11 22:39:28 +0530

    syntax error fix

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -7,12 +7,7 @@ import './Form.css'
 
 const RECAPTCHA_KEY = process.env.APP_SITE_RECAPTCHA_KEY
 if (typeof RECAPTCHA_KEY === 'undefined') {
-  throw new Error(`
-  Env var APP_SITE_RECAPTCHA_KEY is undefined! 
-  You probably forget to set it in your Netlify build environment variables. 
-  Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
-  Note this demo is specifically for Recaptcha v2
-  `)
+  throw new Error(`Env var APP_SITE_RECAPTCHA_KEY is undefined! You probably forget to set it in your Netlify build environment variables. Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings Note this demo is specifically for Recaptcha v2 `)
 }
 
 class Form extends React.Component {
