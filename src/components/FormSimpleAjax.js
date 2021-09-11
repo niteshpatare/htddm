55b87c48a4f4263fd7306dc8d2d0a1356a9ee3c0
Nitesh <niteshp27@gmail.com>
2021-09-11 14:51:11 +0530

    added reacptch apikey to js api url

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -59,7 +59,7 @@ class Form extends React.Component {
     return (
       <Fragment>
         <Helmet>
-          <script src="https://www.google.com/recaptcha/api.js" />
+          <script src="https://www.google.com/recaptcha/api.js?render=6LeRr1wcAAAAAEAgh7vwq9dXzPIBdam8odvndYNi" />
         </Helmet>
         <form
           className="Form"
